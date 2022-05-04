6fddf5ead49d1f51e8729c0e600713ccb8b4166d
Gabriel Gomes de Almeida <gabrielda@.ciandt.com>
2022-05-04 00:21:00 -0300

    Finished up FrontEnd

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -7,7 +7,7 @@ export function Widget() {
 
   return  (
     <Popover className="absolute bottom-4 right-4 flex flex-col items-end md:bottom-8 md:right-8">
-      <Popover.Panel>{WidgetForm}</Popover.Panel>
+      <Popover.Panel><WidgetForm /></Popover.Panel>
 
       <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
         <ChatTeardropDots className="w-6 h-6" />
